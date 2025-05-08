45c5e6b36ab4fcdf42ad1cbd3c54f5bfdf775985
sreepriyavallala5 <sreepriyavallala@gmail.com>
2025-05-08 15:20:32 -0500

    Initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,24 @@
+
+import Favorites from './pages/Favorites';
+import './css/App.css';
+import Home from './pages/Home';
+import NavBar from './components/NavBar';
+import { MovieProvider } from "./contexts/MovieContext";
+import { Routes, Route } from 'react-router-dom';
+function App() {
+ 
+  return (  
+    <MovieProvider>
+      <NavBar/>  
+        <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/favorites" element={<Favorites/>}/>
+      </Routes>
+    </main>
+    </MovieProvider>
+    );
+
+}
+
+export default App
---
+++ vite-project/src/pages/Favorites.jsx
@@ -0,0 +1,26 @@
+import "../css/Favorites.css";
+import  { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
+function Favorites() {
+    const { favorites } = useMovieContext();
+    if(favorites) {
+        return(
+            <div className="favorites">
+                <h2>Your Favorites</h2>
+                <div className="movies-grid">
+                    {favorites.map((movie) => (
+                        <MovieCard key={movie.id} movie={movie}/>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+    return <div className="favorites-empty">
+        <h2>
+            No favorite movies yet
+        </h2>
+        <p> Start adding movies to your favorites and they will appear here </p>
+
+    </div>
+}
+export default Favorites;
